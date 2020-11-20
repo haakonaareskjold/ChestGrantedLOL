0cf01f080e96734d285533e0f291cdbe49cd53c1
Haakon <aareskjold.haakon@gmail.com>
2020-11-20 23:39:38 +0100

    changed post values to  and fixed remaining error handling catches

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -24,18 +24,16 @@ class ApiController extends Controller
 
         // phpdotenv script to fetch DDragon json to update to newest version automatically
         $fetch = new Client();
+
         try {
             $response = $fetch->request(
                 'GET',
                 'http://ddragon.leagueoflegends.com/api/versions.json'
             );
-        } catch (ClientException | ServerException $e) {
-            $error = $e->getResponse()->getStatusCode();
-
-            if ($error !== 200) {
-                exit('an error has occurred, please try again later');
-            }
+        } catch (GuzzleException $exception) {
+            return view('error', compact('exception'));
         }
+
 
         $json = $response->getBody();
         $array = json_decode($json, true);
@@ -46,8 +44,8 @@ class ApiController extends Controller
         ($this->validateForm($request));
 
         // form handling
-        $username = $_POST['username'];
-        $server = $_POST['server'];
+        $username = $request['username'];
+        $server = $request['server'];
 
         // Fetches SID from  Summoner-V4 API and writes it to .env file
         $account = 'https://'.$server.'.api.riotgames.com/lol/summoner/v4/summoners/by-name/'.$username.'?api_key='.$_ENV['RGAPI'];
@@ -71,7 +69,7 @@ class ApiController extends Controller
         $ddragon = new Client();
         try {
             $resDdragon = $ddragon->get($champions);
-        } catch (GuzzleException $e) {
+        } catch (GuzzleException $exception) {
             return view('error', compact('exception'));
         }
 
@@ -81,15 +79,13 @@ class ApiController extends Controller
         // fetches champion-mastery V4 API
         $url = 'https://'.$server.'.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/'.getenv('SID').'?api_key='.$_ENV['RGAPI'];
         $client = new Client();
+
         try {
             $res = $client->get($url);
-        } catch (ClientException | ServerException $e) {
-            $error = $e->getResponse()->getStatusCode();
-
-            if ($error !== 200) {
-                exit('an error has occurred, please try again later');
-            }
+        } catch (GuzzleException $exception) {
+            return view('error', compact('exception'));
         }
+
 
         $json = $res->getBody();
         $list = json_decode($json, true);
